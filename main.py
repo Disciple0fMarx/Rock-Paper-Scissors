bf68c516a06ed4ca2f7abc853b80b24ca56aa148
Disciple0fMarx <didou.bah@gmail.com>
2021-07-05 14:34:56 +0100

    Removed unnecessary imports

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.image import Image
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -74,7 +69,6 @@ Builder.load_string("""
 """)
 
 
-# Declare both classes
 class StartWindow(Screen):
     """The start menu"""
     pass
