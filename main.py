44c0171efdb5f662843929e1501458617024559a
Disciple0fMarx <didou.bah@gmail.com>
2021-07-05 12:49:51 +0100

    Added new page + real button functionality

--- main.py
+++ main.py
@@ -5,43 +5,72 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+
+Builder.load_string("""
+<StartWindow>:
+    BoxLayout:
+        orientation: "vertical"
+        Label:
+            text: "Rock Paper Scissors"
+            font_size: 20
+            size_hint: .1, .1
+            pos_hint: {"center_x": .5, "center_y": .5}
+            color: "#03C04A"
+            bold: True
+        Image:
+            source: "favicon.png"
+            pos_hint: {"center_x": .5, "center_y": .5}
+        Button:
+            text: "Start"
+            size_hint: 1, .25
+            pos_hint: {"center_x": .5, "center_y": .5}
+            background_color: "#03C04A"
+            bold: True
+            background_normal: ""
+            font_size: 18
+            on_release: app.root.current = "game"
+<GameWindow>:
+    BoxLayout:
+        orientation: "vertical"
+        Label:
+            text: "You made it here!"
+            color: "#000000"
+        Button:
+            text: "Go back"
+            on_release: app.root.current = "start"
+""")
+
+
+# Declare both classes
+class StartWindow(Screen):
+    """The start menu"""
+    pass
+
+
+class GameWindow(Screen):
+    """The actual game"""
+    pass
 
 
 class PlayGame(App):
-    def start(self, instance):
-        """Takes you to the actual game"""
-        print("The button has been clicked!")
-
     def build(self):
-        """Start menu"""
-        start_menu = BoxLayout(orientation="vertical")
+        """The application we're running"""
         Window.clearcolor = (1, 1, 1, 1)
-        gameTitle = Label(
-            text="Rock Paper Scissors",
-            font_size=20,
-            size_hint=(.1, .1),
-            pos_hint={"center_x": .5, "center_y": .5},
-            color="#03C04A",
-            bold=True
-        )
-        start_menu.add_widget(gameTitle)
-        logo = Image(source="favicon.png", pos_hint={"center_x": .5, "center_y": .5})
-        start_menu.add_widget(logo)
-        startButton = Button(
-            text="Start",
-            size_hint=(1, .25),
-            pos_hint={"center_x": .5, "center_y": .5},
-            background_color="#03C04A",
-            bold=True,
-            background_normal="",
-            font_size=18
-        )
-        startButton.bind(on_press=self.start)
-        start_menu.add_widget(startButton)
-        start_menu.size_hint = (.6, .9)
-        start_menu.pos_hint = {"center_x": .5, "center_y": .5}
-
-        return start_menu
+        screen_manager = ScreenManager()
+        screen = Screen(name="start")
+        self.start_screen = StartWindow()
+        screen.add_widget(self.start_screen)
+        screen_manager.add_widget(screen)
+        self.start_screen.size_hint = (.6, .9)
+        self.start_screen.pos_hint = {"center_x": .5, "center_y": .5}
+        screen = Screen(name="game")
+        self.game_screen = GameWindow()
+        screen.add_widget(self.game_screen)
+        screen_manager.add_widget(screen)
+        return screen_manager
 
 
 if __name__ == "__main__":
